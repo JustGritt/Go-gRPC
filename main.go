cb2d4e061a6d8a328cecdc7c3a27788bab68675f
Charles <charles258@hotmail.fr>
2023-01-03 15:27:51 +0100

    Fix conflict Merge

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"github.com/JustGritt/go-grpc/database"
+	_ "github.com/JustGritt/go-grpc/docs"
 	"github.com/JustGritt/go-grpc/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v4"
-
 	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/gofiber/swagger"
+	"github.com/golang-jwt/jwt/v4" // gin-swagger middleware
 )
 
 func welcome(c *fiber.Ctx) error {
@@ -24,12 +25,10 @@ func restricted(c *fiber.Ctx) error {
 func setupRoutes(app *fiber.App) {
 	// Welcome
 	app.Get("/api", welcome)
+	app.Get("/swagger/*", swagger.HandlerDefault)
 
 	// Login route
 	app.Post("/login", routes.Login)
-
-	// Stream routes
-	// =================
 	app.Get("/api/stream", routes.GetStream)
 
 	app.Use(jwtware.New(jwtware.Config{
@@ -47,40 +46,51 @@ func setupRoutes(app *fiber.App) {
 
 	// GET routes
 	// =================
-	app.Get("/api", welcome) // Welcome
-	app.Get("/api/routess", routes.GetUsers)
-	app.Get("/api/routess/:id", routes.GetUser)
-	app.Get("/api/products", routes.GetProducts)
-	app.Get("/api/products/:id", routes.GetProduct)
-	app.Get("/api/payments", routes.GetPayments)
-	app.Get("/api/payments/:id", routes.GetPayment)
-	// app.Get("/api/payments/routes/:id", routes.GetPaymentsByUser)
+	app.Get("/api/users", restricted, routes.GetUsers)
+	app.Get("/api/users/:id", restricted, routes.GetUser)
+	app.Get("/api/products", restricted, routes.GetProducts)
+	app.Get("/api/products/:id", restricted, routes.GetProduct)
+	app.Get("/api/payments", restricted, routes.GetPayments)
+	app.Get("/api/payments/:id", restricted, routes.GetPayment)
+	// app.Get("/api/payments/user/:id", routes.GetPaymentsByUser)
 	// app.Get("/api/payments/product/:id", routes.GetPaymentsByProduct)
 
 	// POST routes
 	// =================
-	app.Post("/api/routess", routes.CreateUser)
-	app.Post("/api/products", routes.CreateProduct)
-	app.Post("/api/payments", routes.CreatePayment)
+	app.Post("/api/users", restricted, routes.CreateUser)
+	app.Post("/api/products", restricted, routes.CreateProduct)
+	app.Post("/api/payments", restricted, routes.CreatePayment)
 
 	// PUT routes
 	// =================
-	app.Put("/api/routess/:id", routes.UpdateUser)
-	app.Put("/api/products/:id", routes.UpdateProduct)
-	app.Put("/api/payments/:id", routes.UpdatePayment)
+	app.Put("/api/users/:id", restricted, routes.UpdateUser)
+	app.Put("/api/products/:id", restricted, routes.UpdateProduct)
+	app.Put("/api/payments/:id", restricted, routes.UpdatePayment)
 
 	// DELETE routes
 	// =================
-	app.Delete("/api/routess/:id", routes.DeleteUser)
-	app.Delete("/api/products/:id", routes.DeleteProduct)
-	app.Delete("/api/payments/:id", routes.DeletePayment)
+	app.Delete("/api/users/:id", restricted, routes.DeleteUser)
+	app.Delete("/api/products/:id", restricted, routes.DeleteProduct)
+	app.Delete("/api/payments/:id", restricted, routes.DeletePayment)
 
 }
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+// @host      localhost:3000
+// @BasePath  /api/v1
 func main() {
 	database.Connect()
 	app := fiber.New()
 
 	setupRoutes(app)
+
 	app.Listen(":3000")
 }
--- routes/payment.go
+++ routes/payment.go
@@ -28,6 +28,15 @@ func CreateResponsePayments(payments []models.Payment) []Payment {
 	return response
 }
 
+// Get all payments godoc
+// @Summary Get all payments
+// @Description Get all payments
+// @Tags payments
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} []Payment
+// @Failure 400 {string} string "Payment not found"
+// @Router /api/payments [get]
 func GetPayments(c *fiber.Ctx) error {
 	var payments []models.Payment
 	database.Database.Db.Find(&payments)
@@ -39,6 +48,16 @@ func GetPayments(c *fiber.Ctx) error {
 	return c.Status(200).JSON(CreateResponsePayments(payments))
 }
 
+// Get payment by id godoc
+// @Summary Get payment by id
+// @Description Get payment by id
+// @Tags payments
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Payment ID"
+// @Success 200 {object} Payment
+// @Failure 400 {string} string "Payment not found"
+// @Router /api/payments/{id} [get]
 func GetPayment(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
@@ -56,18 +75,51 @@ func GetPayment(c *fiber.Ctx) error {
 	return c.Status(200).JSON(CreateResponsePayment(payment))
 }
 
+// Create a new payment godoc
+// @Summary Create a new payment
+// @Description Create a new payment
+// @Tags payments
+// @Accept  json
+// @Produce  json
+// @Param payment body Payment true "Payment"
+// @Success 200 {object} Payment
+// @Failure 400 {string} string "Something went wrong"
+// @Router /api/payments [post]
 func CreatePayment(c *fiber.Ctx) error {
 	var payment models.Payment
 
 	if err := c.BodyParser(&payment); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
+
+	// Check if product exists
+	var product models.Product
+	database.Database.Db.Where("id = ?", payment.ProductID).Find(&product)
+
+	if product.ID == 0 {
+		return c.Status(404).JSON("Product not found")
+	}
+
+	// Check if the price of the product is the same as the price of the payment
+	if product.Price != payment.Price {
+		return c.Status(400).JSON("Price of the product is not the same as the price of the payment")
+	}
+
 	database.Database.Db.Create(&payment)
 	b.Publish(payment)
-
 	return c.Status(200).JSON(CreateResponsePayment(payment))
 }
 
+// Delete the payment with the given id godoc
+// @Summary Delete the payment with the given id
+// @Description Delete the payment with the given id
+// @Tags payments
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Payment ID"
+// @Success 200 {object} Payment
+// @Failure 400 {string} string "Payment not found"
+// @Router /api/payments/{id} [delete]
 func DeletePayment(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
@@ -86,7 +138,16 @@ func DeletePayment(c *fiber.Ctx) error {
 	return c.Status(200).JSON(CreateResponsePayment(payment))
 }
 
-// Update the payment with the given id
+// Update the payment with the given id godoc
+// @Summary Update the payment with the given id
+// @Description Update the payment with the given id
+// @Tags payments
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Payment ID"
+// @Param payment body Payment true "Payment"
+// @Success 200 {object} Payment
+// @Failure 400 {string} string "Payment not found"
 func UpdatePayment(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 
